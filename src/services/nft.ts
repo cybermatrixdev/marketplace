755ed49049c5e63e70ac8b0f4ca93122a7368418
cybermatrixdev <100643071+cybermatrixdev@users.noreply.github.com>
2022-03-13 10:18:20 -0700

    Update nft.ts

--- src/services/nft.ts
+++ src/services/nft.ts
@@ -6,4 +6,73 @@ const client = createClient("http://localhost:5000", { "Content-type": "applicti
 const list = { "name": "xyz" }
 const listByAddress = { "name": "abc" }
 
+export default function CreateItem() {
+  const [fileUrl, setFileUrl] = useState(null)
+  const [formInput, updateFormInput] = useState({ price: '', name: '', description: '' })
+  const router = useRouter()
+
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(
+        file,
+        {
+          progress: (prog) => console.log(`received: ${prog}`)
+        }
+      )
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      setFileUrl(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
+  async function createMarket() {
+    const { name, description, price } = formInput
+    if (!name || !description || !price || !fileUrl) return
+    /* first, upload to IPFS */
+    const data = JSON.stringify({
+      name, description, image: fileUrl
+    })
+    try {
+      const added = await client.add(data)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      /* after file is uploaded to IPFS, pass the URL to save it on Polygon */
+      createSale(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
+
+  async function createSale(url) {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)    
+    const signer = provider.getSigner()
+
+    /* next, create the item */
+    let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
+    let transaction = await contract.createToken(url)
+    let tx = await transaction.wait()
+    let event = tx.events.length && tx.events[0]
+    let value
+    let tokenId
+    if (event.args && event.args.length > 2) { // local hardhat
+        value = event.args[2]
+        tokenId = value && value.toNumber()
+    } else { // polgon mumbai testnet
+        tokenId = event.transactionIndex
+    }
+    const price = ethers.utils.parseUnits(formInput.price, 'ether')
+
+    /* then list the item for sale on the marketplace */
+    contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    let listingPrice = await contract.getListingPrice()
+    listingPrice = listingPrice.toString()
+
+    transaction = await contract.createMarketItem(nftaddress, tokenId, price, { value: listingPrice })
+    await transaction.wait()
+    router.push('/')
+  }
+}
+
 export const NFTService = { list, listByAddress }
